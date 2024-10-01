3ce68893ae13530d96ef3980d45432ce5d25f910
Khazar <96715344+Khazar98@users.noreply.github.com>
2024-10-01 13:19:45 +0400

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 //TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
-
+aedqwerwqerwqer
 func main() {
   //TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
   // to see how GoLand suggests fixing it.
